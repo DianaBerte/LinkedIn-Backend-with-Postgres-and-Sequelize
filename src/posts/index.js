3e9a6d6029773524b44173278b15e882ea3e399e
DianaBerte <dianaberte.go@gmail.com>
2023-03-31 14:17:39 +0200

    Posts belongsTo Users - working

--- src/posts/index.js
+++ src/posts/index.js
@@ -3,6 +3,7 @@ import createHttpError from "http-errors"
 import UsersModel from "../users/model.js"
 import PostsModel from "./model.js"
 import res from "express/lib/response.js"
+import e from "express"
 
 const postsRouter = express.Router()
 
@@ -10,6 +11,15 @@ postsRouter.post("/:userId/posts", async (request, response, next) => {
     try {
         const { postId } = await PostsModel.create({ ...request.body, userId: request.params.userId })
         response.status(201).send({ postId })
+    } catch (error) {
+        next(error)
+    }
+})
+
+postsRouter.get("/:userId/posts", async (request, response, next) => {
+    try {
+        const posts = await PostsModel.findAll({ where: { userId: request.params.userId }, include: [{ model: UsersModel, attributes: ["name", "surname"] }] })
+        response.send(posts)
     } catch (error) {
         next(error)
     }
