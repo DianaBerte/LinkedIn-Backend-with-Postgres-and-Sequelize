a2e2b75104c0330274ded33539dee332e59549b4
DianaBerte <dianaberte.go@gmail.com>
2023-03-31 12:12:20 +0200

    POST user

--- src/users/index.js
+++ src/users/index.js
@@ -1,7 +1,16 @@
-import express from "express"
+import express, { response } from "express"
 import createHttpError from "http-errors"
 import UsersModel from "./model.js"
 
 const usersRouter = express.Router()
+
+usersRouter.post("/", async (request, response, next) => {
+    try {
+        const { userId } = await UsersModel.create(request.body)
+        response.status(201).send({ userId })
+    } catch (error) {
+        next(error)
+    }
+})
 
 export default usersRouter
